c612f726094c438470bbb45bcc1b194c8c6caca1
AbedinAvdijaj <aa72972@ubt-uni.net>
2026-01-16 14:10:00 +0100

    Add files via upload

---
+++ ushtrimi9.php
@@ -0,0 +1,22 @@
+<?php 
+$name ="Abedin";
+$lastname = "Avdijaj";
+$age = "19";
+$professor = "Blerta";
+echo ($name);
+echo ($lastname);
+echo ($age);
+break
+echo ($professor);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
